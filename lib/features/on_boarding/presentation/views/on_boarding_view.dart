290c91d1d95b5c37c70c0bb7ec24d2a71315af50
saeed-dai-alnoor <saeeddaialnoor@gmail.com>
2025-10-17 11:47:13 +0200

    ....

--- lib/features/on_boarding/presentation/views/on_boarding_view.dart
+++ lib/features/on_boarding/presentation/views/on_boarding_view.dart
@@ -1,11 +1,34 @@
+import 'package:dalel/core/utils/app_assets.dart';
+import 'package:dalel/core/utils/app_string.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
 
 class OnBoardingView extends StatelessWidget {
   const OnBoardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return SafeArea(
+      child: Scaffold(
+        body: Column(children: [Text(AppStrings.skip), OnBoardingView()]),
+      ),
+    );
+  }
+}
+
+class OnBoardingWidgetBody extends StatelessWidget {
+  const OnBoardingWidgetBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: PageView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) {
+          return Column(
+            children: [Image.asset(Assets.assetsImagesOnboarding1)],
+          );
+        },
+      ),
+    );
   }
 }
