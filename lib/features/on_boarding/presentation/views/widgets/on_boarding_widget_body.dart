c0093a9014dcd2d3b6fe410354436cde132e2adf
saeed-dai-alnoor <saeeddaialnoor@gmail.com>
2025-10-18 08:46:50 +0200

    Create on_boarding_model

--- lib/features/on_boarding/presentation/views/widgets/on_boarding_widget_body.dart
+++ lib/features/on_boarding/presentation/views/widgets/on_boarding_widget_body.dart
@@ -1,6 +1,7 @@
-import 'package:dalel/core/utils/app_assets.dart';
 import 'package:dalel/core/utils/app_text_styles.dart';
+import 'package:dalel/features/on_boarding/data/models/on_boarding_model.dart';
 import 'package:dalel/features/on_boarding/presentation/views/widgets/custom_smooth_page_indicator.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class OnBoardingWidgetBody extends StatelessWidget {
@@ -10,44 +11,62 @@ class OnBoardingWidgetBody extends StatelessWidget {
   Widget build(BuildContext context) {
     return SizedBox(
       height: 500,
-      child: PageView.builder(
-        physics: BouncingScrollPhysics(),
-        controller: _controller,
-        itemCount: 3,
-        itemBuilder: (context, index) {
-          return Column(
-            children: [
-              Container(
-                height: 290,
-                width: 343,
-                decoration: BoxDecoration(
-                  image: DecorationImage(
-                    image: AssetImage(Assets.assetsImagesOnboarding1),
-                    fit: BoxFit.fill,
+      child: Listener(
+        onPointerSignal: (event) {
+          if (event is PointerScrollEvent) {
+            if (event.scrollDelta.dy > 0) {
+              _controller.nextPage(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            } else if (event.scrollDelta.dy < 0) {
+              _controller.previousPage(
+                duration: Duration(milliseconds: 300),
+                curve: Curves.easeInOut,
+              );
+            }
+          }
+        },
+
+        child: PageView.builder(
+          physics: BouncingScrollPhysics(),
+          controller: _controller,
+          itemCount: onBoardingData.length,
+          itemBuilder: (context, index) {
+            return Column(
+              children: [
+                Container(
+                  height: 290,
+                  width: 343,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: AssetImage(onBoardingData[index].imagePath),
+                      fit: BoxFit.fill,
+                    ),
                   ),
                 ),
-              ),
-              SizedBox(height: 24),
-              CustomSmoothPageIndicator(controller: _controller),
-              SizedBox(height: 32),
-              Text(
-                "Explore The history with Dalel in a smart way",
-                style: CustomTextStyles.poppins500style24.copyWith(
-                  fontWeight: FontWeight.bold,
+                SizedBox(height: 24),
+                CustomSmoothPageIndicator(controller: _controller),
+                SizedBox(height: 32),
+                Text(
+                  onBoardingData[index].title,
+                  style: CustomTextStyles.poppins500style24.copyWith(
+                    fontWeight: FontWeight.bold,
+                  ),
+                  maxLines: 2,
+                  overflow: TextOverflow.ellipsis,
+                  textAlign: TextAlign.center,
                 ),
-                maxLines: 2,
-                overflow: TextOverflow.ellipsis,
-                textAlign: TextAlign.center,
-              ),
-              SizedBox(height: 16),
-              Text(
-                "Explore The history with Dalel in a smart way",
-                style: CustomTextStyles.poppins300style16,
-                textAlign: TextAlign.center,
-              ),
-            ],
-          );
-        },
+                SizedBox(height: 16),
+                Text(
+                  onBoardingData[index].subTitle,
+                  style: CustomTextStyles.poppins300style16,
+                  textAlign: TextAlign.center,
+                ),
+              ],
+            );
+          },
+        ),
       ),
     );
   }
